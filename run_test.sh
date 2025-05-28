d2908122b864e532f82927dd62e7d20db8743d8e
Strivin0311 <hyp@smail.nju.edu.cn>
2025-05-28 13:41:47 +0000

    added the env to test script for nvlink in low-latency mode

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,8 @@
 
 export NVSHMEM_IB_ENABLE_IBGDA=1
 export NVSHMEM_IBGDA_NIC_HANDLER=gpu
+export NVSHMEM_DISABLE_P2P=1 # set to 0 to enable NVLink in low-latency mode
+# export NVSHMEM_SYMMETRIC_SIZE=2**30 # default: 1GB
 
 export DEEPEP_TEST_INTRANODE_LOW_LATENCY=0
 
