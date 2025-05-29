22bbca0274cec84a921c8cd0ff1ec9dd6e48f7c3
Strivin0311 <hyp@smail.nju.edu.cn>
2025-05-29 09:02:28 +0000

    added example2,3

--- nvshmem_tutorials/script/build_and_run_example2.sh
+++ nvshmem_tutorials/script/build_and_run_example2.sh
@@ -2,14 +2,15 @@
 BUILD_ROOT=./build
 SRC_ROOT=./src
 
-SRC_NAME=example1_approx_pi
+SRC_NAME=example2_ring_shift
 
 NUM_PES=8
 
 NVCC_GENCODE="arch=compute_90,code=sm_90"
 
 nvcc -rdc=true -ccbin g++ -gencode=$NVCC_GENCODE \
--I $NVSHMEM_DIR/include -L $NVSHMEM_DIR/lib \
+-I $NVSHMEM_DIR/include \
+-L $NVSHMEM_DIR/lib \
 -lnvshmem -lnvidia-ml -lcuda -lcudart \
 -o $BUILD_ROOT/$SRC_NAME $SRC_ROOT/$SRC_NAME.cu \
 
---
+++ nvshmem_tutorials/src/example2_ring_shift.cu
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <iostream>
+#include <cuda.h>
+
+#include <nvshmem.h>
+#include <nvshmemx.h>
+
+
+__global__ void ring_shift(int* nvs_msg) {
+    int mype = nvshmem_my_pe();
+    int npes = nvshmem_n_pes();
+    int peer = (mype + 1) % npes;
+
+    // send `mype` id as message to my next peer
+    nvshmem_int_p(nvs_msg, mype, peer);
+}
+
+
+int main(int argc, char** argv) {
+    int mype_node, npes_node, msg;
+    cudaStream_t stream;
+
+    // init nvshmem
+    nvshmem_init();
+
+    // get my PE number and number of PEs
+    mype_node = nvshmem_team_my_pe(NVSHMEMX_TEAM_NODE);
+    npes_node = nvshmem_team_n_pes(NVSHMEMX_TEAM_NODE);
+
+    std::cout << "[PE" << mype_node << "] " << "Hello, World! And the number of PEs: " << npes_node << std::endl;
+
+    // set device and stream
+    cudaSetDevice(mype_node);
+    cudaStreamCreate(&stream);
+
+    // allocate memory on the symmetric heap
+    int* nvs_msg = (int*) nvshmem_malloc(sizeof(int));
+
+    // launch kernel
+    ring_shift<<<1, 1, 0, stream>>>(nvs_msg);
+
+    // barrier the stream to ensure all nvshmem ops are completed
+    // i.e. nvs_msg is prepared
+    nvshmemx_barrier_all_on_stream(stream);
+
+    // copy msg to host
+    cudaMemcpyAsync(&msg, nvs_msg, sizeof(int), cudaMemcpyDeviceToHost, stream);
+
+    // cpu sync all ops on stream to be finished
+    // i.e. msg is prepared
+    cudaStreamSynchronize(stream);
+
+    // print msg
+    printf("[PE%d] received message: %d\n", mype_node, msg);
+
+    // free memory on the symmetric heap
+    nvshmem_free(nvs_msg);
+
+    // finalize nvshmem
+    nvshmem_finalize();
+
+    return 0;
+}
