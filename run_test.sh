976f08cfa78de19249222280a451bf4854d34d06
Strivin0311 <hyp@smail.nju.edu.cn>
2025-05-29 06:42:10 +0000

    added nvshmrun install cmds

--- run_test.sh
+++ run_test.sh
@@ -6,8 +6,8 @@ export NVSHMEM_DISABLE_P2P=1 # set to 0 to enable NVLink in low-latency mode
 
 export DEEPEP_TEST_INTRANODE_LOW_LATENCY=0
 
-python tests/test_intranode.py > test_intranode.log 2>&1
+# python tests/test_intranode.py > test_intranode.log 2>&1
 
 # python tests/test_internode.py > test_internode.log 2>&1
 
-# python tests/test_low_latency.py > test_low_latency.log 2>&1
+python tests/test_low_latency.py > test_low_latency.log 2>&1
