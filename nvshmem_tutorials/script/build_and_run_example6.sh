5fa5cce4243602270c71d8171f16b11af5372e2a
Strivin0311 <hyp@smail.nju.edu.cn>
2025-05-30 11:08:52 +0000

    added example6

---
+++ nvshmem_tutorials/script/build_and_run_example6.sh
@@ -0,0 +1,26 @@
+
+BUILD_ROOT=./build
+SRC_ROOT=./src
+
+SRC_NAME=example6_ring_shift_block
+
+NUM_PES=8
+NUM_RANKS=$NUM_PES
+
+NVCC_GENCODE="arch=compute_90,code=sm_90"
+
+nvcc -rdc=true -ccbin mpicxx -gencode=$NVCC_GENCODE \
+-I $NVSHMEM_DIR/include -I $MPI_HOME/include \
+-L $NVSHMEM_DIR/lib -L $MPI_HOME/lib \
+-lnvshmem -lnvidia-ml -lcuda -lcudart \
+-o $BUILD_ROOT/$SRC_NAME $SRC_ROOT/$SRC_NAME.cu
+
+export NVSHMEMTEST_USE_MPI_LAUNCHER=0
+
+if [[ $NVSHMEMTEST_USE_MPI_LAUNCHER -eq 1 ]]; then
+    mpirun --allow-run-as-root -np $NUM_RANKS $BUILD_ROOT/$SRC_NAME
+else
+    nvshmrun -np $NUM_PES $BUILD_ROOT/$SRC_NAME
+fi
+
+
