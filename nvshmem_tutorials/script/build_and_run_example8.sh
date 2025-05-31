555a0ba3a61a9695d320c04aa1bdc5fdfa8a78e1
Strivin0311 <hyp@smail.nju.edu.cn>
2025-05-31 11:01:22 +0000

    added example8

---
+++ nvshmem_tutorials/script/build_and_run_example8.sh
@@ -0,0 +1,19 @@
+
+BUILD_ROOT=./build
+SRC_ROOT=./src
+
+SRC_NAME=example8_ring_reduce_block
+
+NUM_PES=8
+
+NVCC_GENCODE="arch=compute_90,code=sm_90"
+
+nvcc -rdc=true -ccbin mpicxx -gencode=$NVCC_GENCODE \
+-I $NVSHMEM_DIR/include -I $MPI_HOME/include \
+-L $NVSHMEM_DIR/lib -L $MPI_HOME/lib \
+-lnvshmem -lnvidia-ml -lcuda -lcudart \
+-o $BUILD_ROOT/$SRC_NAME $SRC_ROOT/$SRC_NAME.cu
+
+nvshmrun -np $NUM_PES $BUILD_ROOT/$SRC_NAME
+
+
