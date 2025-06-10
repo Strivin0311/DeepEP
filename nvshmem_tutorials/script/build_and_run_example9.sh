265a1f0111349ed61afa46932bdeb6dc273bdeb2
Strivin0311 <hyp@smail.nju.edu.cn>
2025-06-10 02:57:46 +0000

    added NVSHMEM_HOME env var

--- nvshmem_tutorials/script/build_and_run_example9.sh
+++ nvshmem_tutorials/script/build_and_run_example9.sh
@@ -9,8 +9,8 @@ NUM_PES=8
 NVCC_GENCODE="arch=compute_90,code=sm_90"
 
 nvcc -rdc=true -ccbin mpicxx -gencode=$NVCC_GENCODE \
--I $NVSHMEM_DIR/include -I $MPI_HOME/include \
--L $NVSHMEM_DIR/lib -L $MPI_HOME/lib \
+-I $NVSHMEM_HOME/include -I $MPI_HOME/include \
+-L $NVSHMEM_HOME/lib -L $MPI_HOME/lib \
 -lnvshmem -lnvidia-ml -lcuda -lcudart \
 -o $BUILD_ROOT/$SRC_NAME $SRC_ROOT/$SRC_NAME.cu
 
