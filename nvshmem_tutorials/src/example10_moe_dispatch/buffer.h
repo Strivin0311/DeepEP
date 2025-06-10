a6b43a0b9c360756285f119d5266c7350f957a19
Strivin0311 <hyp@smail.nju.edu.cn>
2025-06-10 10:05:47 +0000

    polished and updated example10 code

--- nvshmem_tutorials/src/example10_moe_dispatch/buffer.h
+++ nvshmem_tutorials/src/example10_moe_dispatch/buffer.h
@@ -9,22 +9,6 @@
 
 namespace ship {
 
-    // Host Buffer class
-    template <typename T> struct HostBuffer {
-        uint32_t size;
-        T *data = nullptr;
-
-        HostBuffer(const std::vector<T> &a) {
-            size = a.size();
-            cudaMallocHost(&data, size * sizeof(T));
-            cudaMemcpy(data, a.data(), size * sizeof(T), cudaMemcpyHostToDevice);
-        }
-
-        const T *get() const { return data; }
-        T *get() { return data; }
-    };
-
-    // Device Buffer class
     template <typename T> struct DeviceBuffer {
         uint32_t size;
         T *data = nullptr;
@@ -37,37 +21,8 @@ namespace ship {
 
         const T *get() const { return data; }
         T *get() { return data; }
-    };
 
-    template <typename T> struct Stride1D {
-        T *data;
-        size_t strideElem;
-    
-        template <typename S>
-        Stride1D(DeviceBuffer<S> &data, size_t strideElem)
-            : data(reinterpret_cast<T *>(data.get())),
-            strideElem(strideElem) {}
-    
-        Stride1D(T *data, size_t strideElem)
-            : data(data),
-            strideElem(strideElem) {}
-    };
-    
-    template <typename T> struct Stride2D {
-        T *data;
-        size_t strideElem;
-        size_t strideRow;
-    
-        template <typename S>
-        Stride2D(DeviceBuffer<S> &data, size_t strideElem, size_t strideRow)
-            : data(reinterpret_cast<T *>(data.get())),
-            strideElem(strideElem),
-            strideRow(strideRow) {}
-    
-        Stride2D(T *data, size_t strideElem, size_t strideRow)
-            : data(data),
-            strideElem(strideElem),
-            strideRow(strideRow) {}
+        ~DeviceBuffer() { cudaFree(data); }
     };
 
 } // namespace ship
